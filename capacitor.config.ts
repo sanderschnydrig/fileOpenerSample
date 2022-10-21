51a697df98a343e7bbf8763ed685370dd87113ce
Sander Schnydrig <s.schnydrig@mengisgruppe.ch>
2022-10-21 09:45:43 +0200

    capacitor files

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'sampleFileOpener',
+  webDir: 'dist/sample-file-opener',
+  bundledWebRuntime: false
+};
+
+export default config;
